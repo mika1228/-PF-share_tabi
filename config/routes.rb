4a170e3dcb99fc778c0371f6869719d26aabbd8a
mika1228 <mika.1993.12.28@gmail.com>
2022-05-18 14:24:16 +0000

    [Update]ログイン画面

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+  
+  namespace :admin do
+    resources :categories, only: [:index, :create, :edit, :update, :destroy]
+  end
+  
+  
+  devise_for :users, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  
+  scope module: :public do
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
