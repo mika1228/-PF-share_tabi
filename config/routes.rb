8e18d3eb4a8e1ed01432d4e780c7fcbd0e807526
mika1228 <mika.1993.12.28@gmail.com>
2022-05-20 17:50:29 +0000

    [Update]トップページ

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   }
   
   scope module: :public do
+    root to: "homes#top"
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
