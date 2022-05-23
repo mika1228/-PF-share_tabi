b29945940f7b19ca9ecf3931594a956d3c4b41b6
mika1228 <mika.1993.12.28@gmail.com>
2022-05-23 14:15:09 +0000

    [Update]管理者画面

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,2 +1,13 @@
 class Admin::UsersController < ApplicationController
+  
+  def index
+    @users = User.all
+    @users = User.page(params[:page]).per(10)
+  end
+  
+  def show
+    @user = User.find(params[:id])
+    @posts = @user.posts.page(params[:page]).per(6)
+  end
+  
 end
